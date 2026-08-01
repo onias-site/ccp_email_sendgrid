0c0bae118acbec4fc8862e3cfc2181945fbff38b
ccpjobsnow <69213361+ccpjobsnow@users.noreply.github.com>
2026-07-31 23:55:09 -0300

    refatoracao completa do login

--- src/main/java/com/ccp/implementations/email/sendgrid/CcpSendGridEmailSender.java
+++ src/main/java/com/ccp/implementations/email/sendgrid/CcpSendGridEmailSender.java
@@ -7,7 +7,7 @@ import com.ccp.especifications.email.CcpEmailSender;
  * Provedor de DI que expõe {@code SendGridEmailSender} como implementação de {@code CcpEmailSender}.
  */
 public class CcpSendGridEmailSender implements CcpInstanceProvider<CcpEmailSender> {
-	
+
 	public CcpEmailSender getInstance() {
 		return new SendGridEmailSender();
 	}
